992f3ad74dfae49fbba3ae12bab33bc5ff76294e
Robert Dodd <robert@dodd.me>
2024-06-26 11:43:02 +1000

    feat: update hidden_focusables example

--- examples/hidden_focusables.rs
+++ examples/hidden_focusables.rs
@@ -11,9 +11,12 @@ fn main() {
         .add_systems(Startup, startup)
         .add_systems(
             Update,
-            handle_click_events
-                .after(UiNavSet)
-                .run_if(on_event::<UiNavClickEvent>()),
+            (
+                handle_keys,
+                handle_click_events
+                    .after(UiNavSet)
+                    .run_if(on_event::<UiNavClickEvent>()),
+            ),
         )
         .run();
 }
@@ -25,44 +28,68 @@ enum ButtonAction {
     Panic,
 }
 
+#[derive(Component, Debug, Clone)]
+struct ButtonWrapper;
+
 fn startup(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
 
     root_full_screen_centered(&mut commands, |p| {
         spawn_menu(true, false, p, (), |p| {
-            menu_buttoni(
-                p,
-                "Hidden via Display",
-                (Focusable::default(), ButtonAction::Panic),
-                |b: &mut ButtonBundle| {
-                    b.style.display = Display::None;
-                },
-            );
-            menu_buttoni(
-                p,
-                "Hidden via Visibility",
-                (Focusable::default(), ButtonAction::Panic),
-                |b: &mut ButtonBundle| {
-                    b.visibility = Visibility::Hidden;
-                },
-            );
-            menu_buttoni(
-                p,
-                "Hidden via 0 size",
-                (Focusable::default(), ButtonAction::Panic),
-                |b: &mut ButtonBundle| {
-                    b.style.max_width = Val::ZERO;
-                    b.style.max_height = Val::ZERO;
-                    b.style.padding = UiRect::ZERO;
-                    b.style.border = UiRect::ZERO;
-                    b.style.overflow = Overflow::clip();
-                },
-            );
-            menu_button(p, "Button 1", true, false, false, ButtonAction::Ok);
-            menu_button(p, "Button 2", false, false, false, ButtonAction::Ok);
-            menu_button(p, "Quit", false, true, false, ButtonAction::Quit);
+            text_widget(p, FontSize::Small, "Press <TAB> to reveal focusables");
+            button_wrapper_widget(p, |p| {
+                menu_buttoni(
+                    p,
+                    "Hidden via Display",
+                    (Focusable::default(), ButtonAction::Panic),
+                    |b: &mut ButtonBundle| {
+                        b.style.display = Display::None;
+                    },
+                );
+                menu_buttoni(
+                    p,
+                    "Hidden via Visibility",
+                    (Focusable::default(), ButtonAction::Panic),
+                    |b: &mut ButtonBundle| {
+                        b.visibility = Visibility::Hidden;
+                    },
+                );
+                menu_buttoni(
+                    p,
+                    "Hidden via 0 size",
+                    (Focusable::default(), ButtonAction::Panic),
+                    |b: &mut ButtonBundle| {
+                        b.style.max_width = Val::ZERO;
+                        b.style.max_height = Val::ZERO;
+                        b.style.padding = UiRect::ZERO;
+                        b.style.border = UiRect::ZERO;
+                        b.style.overflow = Overflow::clip();
+                    },
+                );
+                menu_button(p, "Button 1", true, false, false, ButtonAction::Ok);
+                menu_button(p, "Button 2", false, false, false, ButtonAction::Ok);
+                menu_button(p, "Quit", false, true, false, ButtonAction::Quit);
+            });
         });
     });
+}
+
+/// Utility that spawns a nav menu.
+pub fn button_wrapper_widget(parent: &mut ChildBuilder, children: impl FnOnce(&mut ChildBuilder)) {
+    parent
+        .spawn((
+            ButtonWrapper,
+            NodeBundle {
+                style: Style {
+                    flex_direction: FlexDirection::Column,
+                    width: Val::Percent(100.),
+                    display: Display::None,
+                    ..default()
+                },
+                ..default()
+            },
+        ))
+        .with_children(children);
 }
 
 fn handle_click_events(
@@ -79,6 +106,17 @@ fn handle_click_events(
                 }
                 ButtonAction::Ok => (),
                 ButtonAction::Panic => panic!("Button should not have been clicked!"),
+            };
+        }
+    }
+}
+
+fn handle_keys(keys: Res<ButtonInput<KeyCode>>, mut query: Query<&mut Style, With<ButtonWrapper>>) {
+    if keys.just_pressed(KeyCode::Tab) {
+        if let Ok(mut style) = query.get_single_mut() {
+            style.display = match style.display {
+                Display::None => Display::Flex,
+                _ => Display::Flex,
             };
         }
     }
